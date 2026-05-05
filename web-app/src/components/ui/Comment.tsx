a953870377405c9a3e509035f669664615290589
IvanKholoshyn <ivan.kholoshyn2006@gmail.com>
2026-05-05 17:49:50 +0300

    Added location to comments

--- web-app/src/components/ui/Comment.tsx
+++ web-app/src/components/ui/Comment.tsx
@@ -1,3 +1,7 @@
+import { getAddressByCoords } from "@/data/queries/address";
+import type { Address } from "@/types/address";
+import { useState, useEffect, use } from "react";
+
 interface CommentProps {
     comment: any;
     depth: number;
@@ -6,9 +10,22 @@ interface CommentProps {
 }
 
 export const Comment = ({ comment, depth, author, showId }: CommentProps) => {
+    const [address, setAddress] = useState<Address | null>(null);
+
+    async function fetchAddress() {
+        if (comment.latitude && comment.longitude) {
+            const fetchedAddress = await getAddressByCoords(comment.latitude, comment.longitude);
+            setAddress(fetchedAddress);
+            console.log(`Fetched address for comment ${comment.id}: ${fetchedAddress.display_name}`); // Debug alert
+        }
+    }
+
+    useEffect(() => {
+        fetchAddress();
+    }, []);
 
     return (
-        <div className={`bg-white rounded-2xl shadow-sm pl-4 pb-2 pt-2 pr-4 flex flex-col hover:shadow-lg transition-shadow ml-${depth*2} mt-1`} key={comment.id}>
+        <div className={`bg-white rounded-2xl shadow-sm pl-4 pb-2 pt-2 pr-4 flex flex-col hover:shadow-lg transition-shadow ml-${depth*2} mt-2`} key={comment.id}>
             <p className="text-gray-900 font-semibold">{comment.author.userName}
                 {showId && <span className="text-gray-600 text-sm ml-2 font-normal">User ID: {comment.author.id}</span>}
                 {comment.parentCommentId && (
@@ -26,6 +43,9 @@ export const Comment = ({ comment, depth, author, showId }: CommentProps) => {
             </p>
             {showId && <p className="text-gray-600 text-sm">Comment ID: {comment.id}</p>}
             <p className="text-gray-900">{comment.commentMessage}</p>
+            {comment.latitude && comment.longitude && (
+                <p className="text-gray-600 text-sm mt-1">{address?.address.road}{address?.address.house_number ? `, ${address?.address.house_number}` : ''}</p>
+            )} 
             {comment.replies.map((reply: any) => (
                 <Comment comment={reply} depth={depth + 1} author={comment.author} showId={showId} key={reply.id} />
             ))}
