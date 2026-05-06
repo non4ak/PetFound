d6c6f0fb90a5dbbd872e2a2e860244cf3860d6a8
IvanKholoshyn <ivan.kholoshyn2006@gmail.com>
2026-05-07 00:02:43 +0300

    Created page and route for comments management

--- web-app/src/data/queries/comments.ts
+++ web-app/src/data/queries/comments.ts
@@ -1,5 +1,5 @@
 import axiosClient from "@/api/axios-client";
-import type { GetCommentsRespond, PagedList, CommentDto } from "@/types/comments";
+import type { GetCommentsRespond, PagedList, CommentDto, CommentSecondDto } from "@/types/comments";
 
 /*export async function getAnnouncements() {
     
@@ -11,6 +11,12 @@ import type { GetCommentsRespond, PagedList, CommentDto } from "@/types/comments
 
 export async function getCommentsByAnnouncementId(id: number) {
     const response = await axiosClient.get<GetCommentsRespond<PagedList<CommentDto>>>(`/announcements/${id}/comments`);
+
+    return response.data.data;
+}
+
+export async function getAllComments() {
+    const response = await axiosClient.get<GetCommentsRespond<PagedList<CommentSecondDto>>>(`/admin/comments`);
 
     return response.data.data;
 }
